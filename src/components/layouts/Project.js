ee9459afaaba79f5e6b8494f27082f9aad52b44f
Hashir-Z <hashir2k@yahoo.com>
2024-10-27 01:06:28 -0500

    Fixed up images, finalized touches

--- src/components/layouts/Project.js
+++ src/components/layouts/Project.js
@@ -1,15 +1,37 @@
-import React from 'react'
+// import React from 'react'
+
+// const Project = ({ id, name, url, skills }) => {
+//     return (
+//         <div data-aos="fade-up" className=" col-12 col-lg-4 project-card">
+//             <div className="image-project">
+//                 <div className={`img-pro ${id}`}></div>
+//             </div>
+//             <a title={url} className="project-name" rel="noopener noreferrer" target="_blank" href={url}><h2>{name}</h2></a>
+//             {skills && <h3>{skills.map(skill => skill)}</h3>}
+//         </div>
+//     )
+// }
+
+// export default Project
+
+import React from 'react';
 
 const Project = ({ id, name, url, skills }) => {
+    const handleImageClick = () => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
+
     return (
-        <div data-aos="fade-up" className=" col-12 col-lg-4 project-card">
-            <div className="image-project">
+        <div data-aos="fade-up" className="col-12 col-lg-4 project-card">
+            <div className="image-project" onClick={handleImageClick}>
                 <div className={`img-pro ${id}`}></div>
             </div>
-            <a title={url} className="project-name" rel="noopener noreferrer" target="_blank" href={url}><h2>{name}</h2></a>
+            <a title={url} className="project-name" rel="noopener noreferrer" target="_blank" href={url}>
+                <h2>{name}</h2>
+            </a>
             {skills && <h3>{skills.map(skill => skill)}</h3>}
         </div>
-    )
-}
+    );
+};
 
-export default Project
+export default Project;
